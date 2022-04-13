414f0a6ae7c68ba992cc55937b4dad2d96aeac64
Nkomo Butts <nkomo.butts@turner.com>
2022-04-13 02:51:46 -0400

    Modify Hapi start-up to route to React client

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const Path = require('path');
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
 
 var port = process.env.PORT || '4000';
 
 // Create a server with a host and port
 const server = Hapi.server({
-    host: 'localhost',
+//    host: 'localhost',
     port: port
 });
 
@@ -18,10 +20,36 @@ server.route({
         return 'hello world';
     }
 });
-
+/*
+server.route({
+	method: 'GET',
+	path: '/app/{path*}',
+	handler: {
+	    directory: {
+		path: Path.join(__dirname, '../client/build/'),
+		listing: false,
+		index: true
+	    }
+	}
+});
+*/
 // Start the server
 async function start() {
     try {
+	await server.register(Inert);
+
+	server.route({
+	    method: 'GET',
+	    path: '/app/{path*}',
+	    handler: {
+		directory: {
+		    path: Path.join(__dirname, '../client/build/'),
+		    listing: false,
+		    index: true
+		}
+	    }
+	});
+	
         await server.start();
     } catch (err) {
         console.log(err);
