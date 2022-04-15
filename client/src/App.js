90f239f8f62e4d86ef371f7f6baa759a5f0ce668
niknab <nabuga@uga.edu>
2022-04-15 09:14:46 -0400

    check in

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import {
     Route,
     Routes
 } from "react-router-dom";
+import ContextConnector from "./config/connector";
 
 // Custom defined contexts
 import LogoutContext from "./context/LogoutContext";
@@ -33,50 +34,51 @@ import ForgotPassword from "./components/access/ForgotPassword";
 
 // <Navigate exact from="/login" to="/catalog" />
 
-function App()
-{
+function App(){
     const queryClient = new QueryClient();
     const [isLoggedOut, setIsLoggedOut] = useState( true );
     const [disableNavigation, setDisableNavigation] = useState( false );
 
-    if (isLoggedOut) {
-        return (
+    // if (isLoggedOut) {
+    return (
+        <ContextConnector>
             <QueryClientProvider client={queryClient}>
                 <LogoutContext.Provider value={{ isLoggedOut }}>
                     <Router>
-                        <AppContext.Provider value={{ disableNavigation }}>
+                        {/* <AppContext.Provider value={{ disableNavigation }}> */}
                             <TopMost>
                                 <Routes>
+                                    <Route exact path="/" element={<Catalog />} />
                                     <Route exact path="/verify" element={<VerifyEmailAddress />} />
                                     <Route exact path="/reset" element={<ForgotPassword />} />
                                     <Route exact path="/signin" element={<SignIn />} />
                                     <Route exact path="/signup" element={<SignUp />} />
-                                    <Route exact path="/" element={<Catalog />} />
                                     <Route exact path="/book" element={<CatalogD />} />
                                     <Route exact path="/inventory" element={<Inventory />} />
                                     <Route exact path="/cart" element={<Cart />} />
                                     <Route path="/merchant" element={ <MerchantViewContainer /> } />
                                 </Routes>
                             </TopMost>
-                        </AppContext.Provider>
+                        {/* </AppContext.Provider> */}
                     </Router>
                 </LogoutContext.Provider>
             </QueryClientProvider>
-        );
-    } // if (isLoggedOut)
-    else {
-        return (
-            <QueryClientProvider client={queryClient}>
-                <LogoutContext.Provider value={{isLoggedOut}}>
-                    <div className="App">
-                        <header className="App-header">
-                            <h1>Logged In</h1>
-                        </header>
-                    </div>
-                </LogoutContext.Provider>
-            </QueryClientProvider>
-        )
-    } // else (isLoggedOut)
+        </ContextConnector>
+    );
+    // } // if (isLoggedOut)
+    // else {
+    //     return (
+    //         <QueryClientProvider client={queryClient}>
+    //             <LogoutContext.Provider value={{isLoggedOut}}>
+    //                 <div className="App">
+    //                     <header className="App-header">
+    //                         <h1>Logged In</h1>
+    //                     </header>
+    //                 </div>
+    //             </LogoutContext.Provider>
+    //         </QueryClientProvider>
+    //     )
+    // } // else (isLoggedOut)
 } // App
 
 export default App;
--- client/src/components/access/SignIn.js
+++ client/src/components/access/SignIn.js
@@ -9,92 +9,119 @@ import {
     Space
 } from 'antd';
 
-const { Title } = Typography;
+import React, { useContext, useState } from "react";
+import Context from "../../context/AppContext";
+import { Navigate } from "react-router-dom";
 
+const { Title } = Typography;
 const signInStyle = {
     marginTop: '20px'
 }
 
 export const SignIn = () => {
+
+    const context = useContext(Context);
+    const { user, getUser } = context;
+    const [name, setName] = useState("");
+    const [picture, setPicture] = useState("");
+    const handleChange1 = (e, { value }) => setName({ value });
+
     const onFinish = (values) => {
-        console.log('Success:', values);
+        const username = values.username
+            .replace(".com", "")
+            .replace(".co", "")
+            .replace(".", "");
+    
+        const user = {
+            username: username,
+            email: values.username,
+            password: values.password,
+            is_admin: false
+        };
+    
+        console.log("user:", user);
+        getUser(user);
     };
 
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
 
-    return (
+    const view = user ? (
+        <Navigate to="/" />
+        ) : (
         <Row style={signInStyle}>
-            <Col span={12} offset={6}>
-                <Space direction="vertical">
-                    <Title>Sign In</Title>
-                    <div>Log into your account to access past orders, and customer information.</div>
-                    <Form
-                        name="basic"
-                        labelCol={{ span: 8 }}
-                        wrapperCol={{ span: 16 }}
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
+        <Col span={12} offset={6}>
+            <Space direction="vertical">
+                <Title>Sign In</Title>
+                <div>Log into your account to access past orders, and customer information.</div>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your username!',
+                            },
+                        ]}
                     >
-                        <Form.Item
-                            label="Username"
-                            name="username"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your username!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
+                        <Input onChange={handleChange1} />
+                    </Form.Item>
 
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your password!',
-                                },
-                            ]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
 
-                        <Form.Item
-                            name="remember"
-                            valuePropName="checked"
-                            wrapperCol={{
-                                offset: 8,
-                                span: 16,
-                            }}
-                        >
-                            <Checkbox>Remember me</Checkbox>
-                            <div>Forgot your password?</div>
-                        </Form.Item>
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                        <div>Forgot your password?</div>
+                    </Form.Item>
 
-                        <Form.Item
-                            wrapperCol={{
-                                offset: 8,
-                                span: 16,
-                            }}
-                        >
-                            <Button type="primary" htmlType="submit">
-                                Sign In
-                            </Button>
-                        </Form.Item>
-                        <div>
-                            Don't have an account? Sign up here.
-                        </div>
-                    </Form>
-                </Space>
-            </Col>
-        </Row>
-    ); // return
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Sign In
+                        </Button>
+                    </Form.Item>
+                    <div>
+                        Don't have an account? Sign up here.
+                    </div>
+                </Form>
+            </Space>
+        </Col>
+    </Row>
+      );
+
+    return view; // return
 
 } // SignIn
 
